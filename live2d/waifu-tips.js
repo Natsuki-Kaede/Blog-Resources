3f64ca284d1bf296683ed308ab43ad5a668deb36
Natsuki-Kaede <xflymci@163.com>
2020-05-23 00:19:26 +0800

    Update waifu-tips.js

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -144,7 +144,7 @@ function loadWidget(config) {
 		if (modelId === null) {
 			// 首次访问加载 指定模型 的 指定材质
 			modelId = 2; // 模型 ID
-			modelTexturesId = 50; // 材质 ID
+			modelTexturesId = 40; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
